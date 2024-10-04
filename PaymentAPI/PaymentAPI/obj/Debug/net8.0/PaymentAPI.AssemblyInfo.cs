b526b880b40cf7c38b53f2a8d1b89be013d6ccd2
Shaolong214 <103941617+Shaolong214@users.noreply.github.com>
2024-10-04 13:53:36 +1000

    enable azure database

--- PaymentAPI/PaymentAPI/obj/Debug/net8.0/PaymentAPI.AssemblyInfo.cs
+++ PaymentAPI/PaymentAPI/obj/Debug/net8.0/PaymentAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PaymentAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f93ca6cc54c91afafe74c4680584db060108c172")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b0275f331db3ed1b5c32ad3712dd0f6e5a93aa0c")]
 [assembly: System.Reflection.AssemblyProductAttribute("PaymentAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PaymentAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
